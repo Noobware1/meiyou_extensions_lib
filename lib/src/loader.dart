e4c1a0b830794365469e049448cdf08f757db30a
Noobware1 <110553648+Noobware1@users.noreply.github.com>
2024-01-26 16:05:21 +0530

    added load functions

--- lib/src/loader.dart
+++ lib/src/loader.dart
@@ -2,7 +2,11 @@ import 'dart:typed_data';
 
 import 'package:dart_eval/dart_eval.dart';
 import 'package:dart_eval/dart_eval_security.dart';
+import 'package:meiyou_extensions_lib/models.dart';
+import 'package:meiyou_extensions_lib/network.dart';
 import 'package:meiyou_extensions_lib/src/bridge_models/crypto_dart/plugin.dart';
+import 'package:meiyou_extensions_lib/src/bridge_models/extension_lib/models/extractor_link.dart';
+import 'package:meiyou_extensions_lib/src/bridge_models/extension_lib/network/network_helper.dart';
 import 'package:meiyou_extensions_lib/src/bridge_models/extension_lib/plugin.dart';
 import 'package:meiyou_extensions_lib/src/bridge_models/html/plugin.dart';
 import 'package:meiyou_extensions_lib/src/bridge_models/okhttp/plugin.dart';
@@ -33,5 +37,30 @@ class ExtensionLoader {
       ..addPlugin(HTMLPlugin())
       ..addPlugin(CryptoDartPlugin())
       ..addPlugin(ExtensionLibPlugin());
+  }
+
+  ExtractorApi loadExtractorApi(
+      String library, String name, ExtractorLink link) {
+    return runtime.executeLib(library, name, [$ExtractorLink.wrap(link)]);
+  }
+
+  HttpSource loadHttpSource(
+      String library, String name, NetworkHelper networkHelper) {
+    return runtime
+        .executeLib(library, name, [$NetworkHelper.wrap(networkHelper)]);
+  }
+
+  ParsedHttpSource loadParsedHttpSource(
+      String library, String name, NetworkHelper networkHelper) {
+    return runtime
+        .executeLib(library, name, [$NetworkHelper.wrap(networkHelper)]);
+  }
+
+  Source loadSource(String library, String name) {
+    return runtime.executeLib(library, name, []);
+  }
+
+  CatalogueSource loadCatalogueSource(String library, String name) {
+    return runtime.executeLib(library, name, []);
   }
 }
