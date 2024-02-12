ffc6d1b80d916308b8c69c6f5654b3a94deaed8e
Noobware1 <110553648+Noobware1@users.noreply.github.com>
2024-02-12 11:16:25 +0530

    L commit

--- lib/src/bridge_models/extension_lib/perference.dart
+++ lib/src/bridge_models/extension_lib/perference.dart
@@ -11,6 +11,9 @@ String _source() {
   HTMLPlugin().addAllExports(buffer);
   ExtensionLibPlugin().addAllExports(buffer, 'preference');
   for (var element in [
+    'export \'src/preference/preferences/dilog_preference.dart\';',
+    'export \'src/preference/preferences/two_state_preference.dart\';',
+    'export \'src/preference/preferences/preference.dart\';',
     'export \'src/preference/shared_preferences.dart\';',
   ]) {
     buffer.writeln(element);
