e81e6a325ee0defaadef43e71b4e8c272462fa4c
Noobware1 <110553648+Noobware1@users.noreply.github.com>
2024-02-25 10:23:57 +0530

    L commit

--- lib/src/preference/preference.dart
+++ lib/src/preference/preference.dart
@@ -10,4 +10,26 @@ abstract class Preference<T> {
   void delete();
 
   T defaultValue();
+
+  /// A preference that should not be exposed in places like backups without user consent.
+  static bool isPrivate(String key) {
+    return key.startsWith(_privateStatePrefix);
+  }
+
+  static String privateKey(String key) {
+    return '$_privateStatePrefix$key';
+  }
+
+  /// A preference used for internal app state that isn't really a user preference
+  /// and therefore should not be in places like backups.
+  static bool isAppState(String key) {
+    return key.startsWith(_appStatePrefix);
+  }
+
+  static String appStateKey(String key) {
+    return '$_appStatePrefix$key';
+  }
+
+  static const _privateStatePrefix = "__PRIVATE_";
+  static const _appStatePrefix = "__APP_STATE_";
 }
