35cccab346bde7c7c413905f8495b72f342c9c96
Noobware1 <110553648+Noobware1@users.noreply.github.com>
2024-03-19 12:29:15 +0530

    L commit

--- lib/src/lib_overrides.dart
+++ lib/src/lib_overrides.dart
@@ -1,19 +1,61 @@
 import 'dart:io';
 
-import 'package:meiyou_extensions_lib/src/network/network_helper.dart';
-import 'package:okhttp/okhttp.dart';
+import 'package:meiyou_extensions_lib/network.dart';
+import 'package:meiyou_extensions_lib/preference.dart';
 
 class ExtensionlibOverrides {
   static String sharedPreferencesDir = Directory.current.path;
 
-  static NetworkHelper networkHelper = _FakeNetworkHelper();
+  static NetworkHelper networkHelper = NetworkHelper(MockNetworkPreferences());
 }
 
-class _FakeNetworkHelper implements NetworkHelper {
+class MockNetworkPreferences implements NetworkPreferences {
   @override
-  OkHttpClient get client => OkHttpClient();
+  Preference<String> defaultUserAgent() => MockPreference('default_useragent',
+      'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/121.0');
 
   @override
-  String get defaultUserAgentProvider =>
-      'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36 Edg/122.0.0.0';
+  Preference<int> dohProvider() => MockPreference('doh', -1);
+
+  @override
+  Preference<bool> verboseLogging() => MockPreference('verbose_logging', false);
+}
+
+class MockPreference<T> implements Preference<T> {
+  final String _key;
+  final T _value;
+
+  MockPreference(this._key, this._value);
+
+  @override
+  T defaultValue() => _value;
+
+  @override
+  void delete() {
+    // TODO: implement delete
+  }
+
+  @override
+  T get() => _value;
+
+  @override
+  bool isSet() {
+    return false;
+  }
+
+  @override
+  String key() {
+    return _key;
+  }
+
+  @override
+  void set(value) {
+    // TODO: implement set
+  }
+
+  @override
+  Stream<T> changes() {
+    // TODO: implement changes
+    throw UnimplementedError();
+  }
 }
